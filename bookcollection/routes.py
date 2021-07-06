4c5207bee671c21b6a9f42879de2dd823aa63151
RGIYER <RGIYER@RGIYER-7410.oradev.oraclecorp.com>
2021-07-06 19:02:33 +0530

    Fixed issues with book name, author name formatting

--- bookcollection/routes.py
+++ bookcollection/routes.py
@@ -13,6 +13,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 from better_profanity import profanity
+from textatistic import Textatistic
 
 
 #nltk.download('wordnet')
@@ -23,63 +24,52 @@ class DataCollection:
     '''
     Class is used for Retrieval and Insertion of Childrens' Books Data
     '''
-    def __init__(self, book_name):
+    def __init__(self, book_name, author_name):
         self.title = book_name.split(", Book")[0]
+        self.author = author_name
         self.url = "https://www.commonsensemedia.org"
         self.kwroute = "book-reviews"
         self.headers = {
                         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
                     }
-        self.firefox_driver_path = "geckodriver.exe"
+        #self.firefox_driver_path = "geckodriver.exe"
         self.book_review_url = "https://www.goodreads.com"
-        self.command = "/search?q="
+        self.command = f"/search?utf8=✓&q={self.getAuthorFormatted()}+{self.getTitleFormatted()}&search_type=books&search[field]=on"
         self.quotes_command = "/work/quotes/"
 
     def getDescription(self):
         book = Book.query.filter(Book.title.like(self.title + "%")).first()
         return book.description
 
-    def getReviewText(self):
-        book = Book.query.filter(Book.title.like(self.title + "%")).first()
-        # if reviews not found in DB try retrieving from web and loading into DB
-        if not self.reviewExistDB(book.id):
-            try:
-                for review in self.getReviewsFromWeb():
-                    review_obj = Review(review_text=review, book=book)
-                    db.session.add(review_obj)
-                    db.session.commit()
-                    print(f"Added Reviews for book {self.title}")
-            except Exception as e:
-                print(f"BooK: {self.title} Exception: {str(e)}")
+    def getAuthorFormatted(self):
+        return self.author.lower().replace(" ", "+")
 
-        reviews = Review.query.filter_by(book_id=book.id)
+    def getTitleFormatted(self):
+        return self.title.lower().replace(" ", "+")
+
+    def getReviewText(self):
         corpus = ""
-        for review in reviews:
-            corpus += review.review_text
+        for review in self.getReviewsFromWeb():
+            corpus += review
         stop_words = stopwords.words('english')
+        stop_words = stop_words + ["book", "story", "tale", "novel", "author"]
         lemmatizer = WordNetLemmatizer()
         lemmatized_words = [lemmatizer.lemmatize(word, pos='v') for word in word_tokenize(corpus.lower()) if
                             word not in stop_words and word.isalnum()]
         final_set = " ".join(lemmatized_words)
         return final_set
+
+    def getQuoteTextRaw(self):
+        # if quotes not found in DB try retrieving from web and loading into DB
+        corpus = ""
+        for quote in self.getQuotesFromWeb():
+            corpus += quote
+        return corpus
 
     def getQuoteText(self):
-        book = Book.query.filter(Book.title.like(self.title + "%")).first()
-        # if quotes not found in DB try retrieving from web and loading into DB
-        if not self.quoteExistDB(book.id):
-            try:
-                for quote in self.getQuotesFromWeb():
-                    quote_obj = Quote(review_text=review, book=book)
-                    db.session.add(quote_obj)
-                    db.session.commit()
-                    print(f"Added Quote for book {self.title}")
-            except Exception as e:
-                print(f"Book: {self.title} Exception: {str(e)}")
-
-        reviews = Review.query.filter_by(book_id=book.id)
         corpus = ""
-        for review in reviews:
-            corpus += review.review_text
+        for quote in self.getQuotesFromWeb():
+            corpus += quote
         stop_words = stopwords.words('english')
         lemmatizer = WordNetLemmatizer()
         lemmatized_words = [lemmatizer.lemmatize(word, pos='v') for word in word_tokenize(corpus.lower()) if
@@ -87,43 +77,60 @@ class DataCollection:
         final_set = " ".join(lemmatized_words)
         return final_set
 
-    def reviewExistDB(self, book_id):
-        return(Review.query.filter_by(book_id=book_id).first())
-
-    def quoteExistDB(self, book_id):
-        return(Quote.query.filter_by(book_id=book_id).first())
-
     def getReviewsFromWeb(self):
-        formatted_title = self.getBookTitleString()
-        review_links_res = requests.get(self.book_review_url + formatted_title)
-        reviews_links_soup = BeautifulSoup(review_links_res.text, features="html.parser")
-        review_divs = reviews_links_soup.findAll("div", {"id": "bookReviews"})[0]
-        review_spans = review_divs.findAll('span', id=re.compile(r"reviewTextContainer\d.*"))
-        for span in review_spans:
-            txtContSpan = span.find("span", id=re.compile(r"freeTextContainer\d.*"))
-            txtSpan = span.find("span", id=re.compile(r"freeText\d.*"))
-            if txtSpan:
-                yield txtSpan.get_text().strip()
-            else:
-                yield txtContSpan.get_text().strip()
+        reviews_links_soup = self.getBookReviewSoup()
+        if reviews_links_soup:
+            #print("test2")
+            review_divs = reviews_links_soup.findAll("div", {"id": "bookReviews"})[0]
+            review_spans = review_divs.findAll('span', id=re.compile(r"reviewTextContainer\d.*"))
+            for span in review_spans:
+                txtContSpan = span.find("span", id=re.compile(r"freeTextContainer\d.*"))
+                txtSpan = span.find("span", id=re.compile(r"freeText\d.*"))
+                if txtSpan:
+                    yield txtSpan.get_text().strip()
+                else:
+                    yield txtContSpan.get_text().strip()
 
     def getQuotesFromWeb(self):
-        formatted_title = self.getBookTitleString
-        quotes_res = requests.get(self.book_review_url + self.quotes_command + formatted_title)
-        quotes_page = BeautifulSoup(quotes_res.text, features="html.parser")
-        quotes_divs = quotes_page.findAll('div', {'class': 'quoteText'})
-        #print(quotes_divs)
-        for quotes in quotes_divs:
-            quote_extracted = quotes.get_text().replace("\n", "").split("―")[0]
-            quote_trimmed = quote_extracted.strip().lstrip("“").rstrip("”")
-            yield quote_trimmed
+        quotes_page = self.getBookQuoteSoup()
+        if quotes_page:
+            quotes_divs = quotes_page.findAll('div', {'class': 'quoteText'})
+            #print(quotes_divs)
+            for quotes in quotes_divs:
+                quote_extracted = quotes.get_text().replace("\n", "").split("―")[0]
+                quote_trimmed = quote_extracted.strip().lstrip("“").rstrip("”")
+                yield quote_trimmed
 
-    def getBookTitleString(self):
-        search_text = self.title.lower().replace(" ", "-")
-        res = requests.get(self.book_review_url + self.command + search_text, headers=self.headers)
-        main_page = BeautifulSoup(res.text, features="html.parser")
-        formatted_title = main_page.findAll('a', {'class': 'bookTitle'})[0]['href'].split("?")[0]
-        return formatted_title
+    def getBookReviewSoup(self):
+        author_links_res = requests.get(self.book_review_url + self.command)
+        #print(self.book_review_url + self.command)
+        author_links_soup = BeautifulSoup(author_links_res.text, features="html.parser")
+        formatted_title = self.title.replace(": ", " \(")
+        #print(formatted_title)
+        #print(self.book_review_url + self.command)
+        pattern = rf"^({formatted_title}).*"
+        print(pattern)
+        book_review_span = author_links_soup.find("span", text=re.compile(pattern, re.IGNORECASE))
+        if book_review_span:
+            book_review_parent_tag = book_review_span.find_parent()
+            book_review_link = book_review_parent_tag["href"]
+            print(book_review_link)
+            review_links_res = requests.get(self.book_review_url + book_review_link)
+            reviews_links_soup = BeautifulSoup(review_links_res.text, features="html.parser")
+            return reviews_links_soup
+        else:
+            return None
+
+    def getBookQuoteSoup(self):
+        reviews_links_soup = self.getBookReviewSoup()
+        book_quote_a_tag = reviews_links_soup.find("a", {"class": "actionLink"}, text="More quotes…")
+        if book_quote_a_tag:
+            book_quote_link = book_quote_a_tag["href"]
+            book_quote_res = requests.get(self.book_review_url + book_quote_link)
+            book_quote_soup = BeautifulSoup(book_quote_res.text, features="html.parser")
+            return book_quote_soup
+        else:
+            return None
 
     def saveImage(self, review_text, file_name):
         try:
@@ -173,42 +180,76 @@ def home_page(page_num):
     if request.method == 'POST' and 'tag' in request.form:
        tag = request.form["tag"]
        search = "%{}%".format(tag)
-       print(f"search: {search}")
+       #print(f"search: {search}")
        books = Book.query.filter(or_(Book.title.like(search), Book.rating.like(search),
                                      Book.age.like(search), Book.author.like(search),
                                      Book.year.like(search))).paginate(per_page=8, error_out=True)
        return render_template('home.html', books=books, tag=tag)
     return render_template('home.html', books=books)
 
-@app.route("/details/<book_name>", methods=['GET', 'POST'])
-def details_page(book_name):
-    bookData = DataCollection(book_name)
+@app.route("/details/<book_name>/<author_name>", methods=['GET', 'POST'])
+def details_page(book_name, author_name):
+    bookData = DataCollection(book_name, author_name)
     description = bookData.getDescription()
     reviewText = bookData.getReviewText()
-#    if reviewText:
-#        bookData.saveImage(reviewText, book_name.lower().replace(" ", "_"))
+    quoteText = bookData.getQuoteText()
+    quoteTextRaw = bookData.getQuoteTextRaw()
+    wordCloudReviewImg = None
+    wordCloudProfanityImg = None
+    grade_level = "graduate"
     if reviewText:
-        stop_words = stopwords.words('english')
-        lemmatizer = WordNetLemmatizer()
-        lemmatized_words = [lemmatizer.lemmatize(word, pos='v') for word in word_tokenize(reviewText.lower()) if
-                            word not in stop_words and word.isalnum()]
+        #print(reviewText)
         profanity.load_censor_words()
-        profane_words = [lemma for lemma in lemmatized_words if lemma in profanity.CENSOR_WORDSET]
-        lemma_review_set = " ".join(lemmatized_words)
+        #print("Loaded censor words")
+        profane_words = [lemma for lemma in word_tokenize(reviewText) if lemma in profanity.CENSOR_WORDSET]
+        #print(profane_words)
         profane_review_set = " ".join(profane_words)
-        wordCloudReviewImg = bookData.getWCImage(lemma_review_set)
-        wordCloudProfanityImg = bookData.getWCImage(profane_review_set)
+        wordCloudReviewImg = bookData.getWCImage(reviewText)
+
         if wordCloudReviewImg:
             wordCloudReviewImg = wordCloudReviewImg.decode('utf-8')
 
-        if wordCloudProfanityImg:
-            wordCloudProfanityImg = wordCloudProfanityImg.decode('utf-8')
-    else:
-        wordCloudReviewImg = None
-        wordCloudProfanityImg = None
+        if profane_review_set:
+            wordCloudProfanityImg = bookData.getWCImage(profane_review_set)
+            if wordCloudProfanityImg:
+                print("Image Generated")
+                wordCloudProfanityImg = wordCloudProfanityImg.decode('utf-8')
 
-#    full_filename = os.path.join(app.config['IMG_DIR'], book_name.lower().replace(" ", "_")+".png")
+    if quoteTextRaw:
+        #print(type(quoteTextRaw))
+        readability_scores = Textatistic(quoteTextRaw).scores
+        try:
+            flesch_score = readability_scores['flesch_score']
+        except Exception as e:
+            print("Textatistic Exception")
+
+        #print(f"Flesch Score is: {flesch_score}")
+        if 90 <= flesch_score <= 100:
+            grade_level = "Grade 5 and less"
+        elif 80 <= flesch_score <= 90:
+            grade_level = "Grade 6"
+        elif 70 <= flesch_score <= 80:
+            grade_level = "Grade 7"
+        elif 60 <= flesch_score <= 70:
+            grade_level = "Grade 8 & 9"
+        elif 50 <= flesch_score <= 60:
+            grade_level = "Grade 10 & 12"
+        elif 30 <= flesch_score <= 50:
+            grade_level = "College"
+        else:
+            grade_level = "Graduate"
+
+    if quoteText:
+        wordCloudQuoteImg = bookData.getWCImage(quoteText)
+        if wordCloudQuoteImg:
+            wordCloudQuoteImg = wordCloudQuoteImg.decode('utf-8')
+    else:
+        wordCloudQuoteImg = None
+
+    #    full_filename = os.path.join(app.config['IMG_DIR'], book_name.lower().replace(" ", "_")+".png")
     return render_template('details.html', book_name=book_name, description=description,
                            review_wordcloud=wordCloudReviewImg,
-                           profane_wordcloud=wordCloudProfanityImg
+                           profane_wordcloud=wordCloudProfanityImg,
+                           quote_wordcloud=wordCloudQuoteImg,
+                           grade_level=grade_level
                            )
